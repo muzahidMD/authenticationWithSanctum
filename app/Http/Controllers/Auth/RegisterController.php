8d462ddd846c8f31253c2bc770987ca913839ebe
muzahidmd <mujahidul.core@gmail.com>
2025-11-02 07:38:19 +0600

    register with direct login system

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -16,12 +16,14 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+        // 1. [done] validate requested data (email should be unique)
         $request->validate([
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed'
         ]);
 
+        // 2. [done] create a new user record in database
         $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
@@ -29,7 +31,14 @@ class RegisterController extends Controller
             'password' => $request->input('password')
         ]);
 
-        return redirect(route('login'));
-        // dd($user);
+        // 3.1. (Optional), after new user, redirect to /login
+        // return redirect()->to('/login');
+        // return redirect(route('login'));
+
+        // 3.2. (Optional), after new user, we can authenticate him and redirect him to dashboard
+        auth()->login($user, true);
+
+        // 3.3. redirect to dashboard
+        return redirect()->route('dashboard');
     }
 }
